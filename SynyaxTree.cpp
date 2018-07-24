e3780f592f7fc962ca280ee3938149285e0d10f6
Jack Jiang <jsc723@berkeley.edu>
2018-07-24 16:14:37 -0700

    ^E ->E

--- SynyaxTree.cpp
+++ SynyaxTree.cpp
@@ -26,18 +26,34 @@ set<int> TreeNode::union_set(set<int>& a, set<int>& b)
 }
 
 bool TreeNode::pgen(set<int> &dep, vector<ProofLine> &proof, int start) {
-	int i = find(dep, this, proof);
+	int i = find(dep, this, proof), j;
+	bool result = false;
 	if (i != -1) {
 		return true;
 	}
 	TreeNode *any = new AnyNode();
+
 	TreeNode *and_left = new AndOpNode(this, any);
 	TreeNode *and_right = new AndOpNode(any, this);
 	if (and_left->helpful(proof) && and_left->pgen(dep, proof, start) ||
 		and_right->helpful(proof) && and_right->pgen(dep, proof, start)) {
-		return true;
+		result = true;
+		goto end;
 	}
-	delete and_left, and_right;
+	
+
+	TreeNode *if_node = new IfOpNode(any, this);
+	i = find(dep, if_node, proof);
+	if (i != -1) {
+		TreeNode *cond = proof[i].formula;
+		if (cond->pgen(dep, proof, start)) {
+			result = true;
+			goto end;
+		}
+	}
+
+end:
+	delete and_left, and_right, if_node;
 	delete any;
 	return false;
 }
